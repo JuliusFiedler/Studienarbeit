0f9eb76766bf955b098f0bcb30562b226c37a43f
Julius <fiedler.julius@mailbox.tu-dresden.de>
2020-08-29 01:08:18 +0200

    sindy simple in python

--- UDE/my_julia_files/Sindy_test.py
+++ UDE/my_julia_files/Sindy_test.py
@@ -3,6 +3,8 @@
 # %%
 import csv
 import numpy as np 
+import sympy as sp
+import itertools as it
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.integrate import odeint
@@ -16,9 +18,9 @@ import pandas as pd
 
 # %%
 # System auswählen
-system = 3 # 1 = volterra, 2 = lorenz, 3 = roessler, 4 = wp
+system = 4 # 1 = volterra, 2 = lorenz, 3 = roessler, 4 = wp
 # NN auswerten?
-NN = True
+NN = False
 
 
 t_end = 3
@@ -28,6 +30,7 @@ if (system == 1):
     p_nom = np.array([[0, 1.3, 0, 0, -0.9, 0], [0, 0, -1.8, 0, 0.8, 0]])
     feature_library = ps.PolynomialLibrary(degree=2)
     feature_names=["x", "y"]
+    n = 2
 elif (system == 2):
     sys_name = "lorenz"
     p_nom = np.array(   [[0, -10, 10, 0, 0, 0, 0, 0, 0, 0], 
@@ -35,6 +38,7 @@ elif (system == 2):
                         [0, 0, 0, -8/3, 0, 1, 0, 0, 0, 0]])
     feature_library = ps.PolynomialLibrary(degree=2)
     feature_names=["x", "y", "z"]
+    n = 3
 elif (system == 3):
     sys_name = "roessler"
     p_nom = np.array(   [[0.0, 0.0, -1.0, -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
@@ -42,10 +46,14 @@ elif (system == 3):
                         [ 0.1, 0.0, 0.0, -5.3, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]])
     feature_library = ps.PolynomialLibrary(degree=2)
     feature_names=["x", "y", "z"]
+    n = 3
     th = 0.05
 elif system == 4:
     sys_name = "wp"
+    n = 4
     t_end = 5
+    m1 = 3.34
+    m2 = 0.8512
     library_functions = [
         lambda x : x,
         lambda x : np.sin(x),
@@ -91,8 +99,163 @@ if NN:
     L_high_res_0_001 = read('L_hr_0_001_' + sys_name + '.csv')
 # t_end = 3
 tt = np.linspace(0, t_end, X.shape[0])
-    
 
+# %%
+
+
+poly_oder = 2
+x1, x2, x3, x4 = sp.symbols('x1, x2, x3, x4')
+x_list = [x1, x2, x3, x4]
+polys = list()
+variables = x_list[0:n]
+for i in range(1, poly_oder+1):
+    comb = list(it.combinations_with_replacement(variables, i))
+    for j in range(len(comb)):
+        monom = comb[j][0]
+        for k in range(1, len(comb[j])):
+            monom *= comb[j][k]
+        polys.append(monom)
+polys.insert(0, 1)
+print(polys)
+print(len(polys))
+
+# %%
+#Verkettung
+def mult(x):
+    temp = 1
+    for i in range(len(x)):
+        temp *= x[i]
+    return temp
+def verkettung():
+    depth = 4 
+    h = [sp.sin(x1), sp.cos(x1), x3]
+    # Zähler bauen
+    nominator = []
+    for i in range(depth):
+        comb = list(it.combinations_with_replacement(h, i+1))
+        for j in range(len(comb)):
+            nominator = np.append(nominator, mult(comb[j]))
+    nominator = np.append(nominator, 1)
+    # Bruch bauen
+    denom = m1+m2*sp.sin(x1)**2
+    basis = np.copy(variables)
+    for z_i in nominator:
+        basis = np.append(basis, z_i/denom)
+    
+    return basis
+
+# %%
+def library(h, u, X):
+    f = sp.lambdify(u, h)
+    if n == 2:
+        l = f(X[:, 0], X[:, 1])
+    elif n == 3:
+        l = f(X[:, 0], X[:, 1], X[:, 2])
+    elif n == 4:
+        l = f(X[:, 0], X[:, 1], X[:, 2], X[:, 3])
+    lib = np.ones((X.shape[0], len(l)))
+    for i in range(len(l)):
+        lib[:, i] = l[i]
+    return lib
+# %%
+def make_sparse(Xi, lam=0.2):
+    was_sparse_already = True
+    for i in range(Xi.shape[0]):
+            for j in range(Xi.shape[1]):
+                if abs(Xi[i][j]) < lam:
+                    if Xi[i][j] != 0:
+                        was_sparse_already = False
+                    Xi[i][j] = 0                    
+    return Xi, was_sparse_already
+# %%
+def calc_Xi_select(Xi, theta, DX):
+    Xi_select = []
+    for i in range(Xi.shape[1]):
+        a = np.array([], dtype=int)
+        for j in range(Xi.shape[0]):
+            if Xi[j][i] != 0:
+                a = np.append(a, j)
+        # print(a)
+        theta_select = theta[:, a]
+        DX_select = DX[:, i]
+        Xi_i = np.linalg.lstsq(theta_select, DX_select, rcond=None)[0]
+        # print(Xi_i)
+        # print(Xi_select)
+        Xi_select = np.append(Xi_select, Xi_i)
+    return Xi_select
+# %%
+def reshape_Xi(Xi, Xi_select):
+    count = 0
+    for i in range(Xi.shape[1]):
+        for j in range(Xi.shape[0]):
+            if Xi[j][i] != 0:
+                Xi[j, i] = Xi_select[count]
+                count += 1
+    return Xi
+# %%
+# def reshape_Xi(Xi, Xi_select):
+#     count = 0
+#     non_zero_r, non_zero_c = np.nonzero(Xi)
+
+#     return Xi
+# %%
+def SIR(Xi, h):
+    for i in range(Xi.shape[1]):
+        eq_ = None
+        for j in range(Xi.shape[0]):
+            if Xi[j][i] != 0:
+                if eq_ == None:
+                    eq_ = round(Xi[j][i], 5) * h[j]
+                else:
+                    eq_ += round(Xi[j][i], 5) * h[j]
+        print("f" + str(i+1) + " = " + str(eq_))
+        print(" = " + str(sp.simplify(eq_)) + "\n")
+# %%
+def sel_lin_indep(theta, basis):
+    is_ = []
+    rangabfall = 0
+    for i in range(1, theta.shape[1]):
+        r_ = np.linalg.matrix_rank(theta[:, 0:i])
+        if r_ < i+1 - rangabfall:
+            is_ = np.append(is_, i)
+            rangabfall += 1
+    basis_ = np.copy(basis)
+    basis_ = np.delete(basis_, is_)
+    return basis_
+# %%
+def sindy_simple(X, DX, h, u, lam=0.2, maxiter=30):
+    theta = library(h, u, X)
+    
+    # initial guess
+    Xi = np.linalg.lstsq(theta, DX, rcond=None)[0]
+    # kleine koeff nullen
+    Xi, _ = make_sparse(Xi, lam)
+    iters = 0
+    for i in range(maxiter):
+        Xi_select = calc_Xi_select(Xi, theta, DX)
+        Xi = reshape_Xi(Xi, Xi_select)
+        Xi, wsa = make_sparse(Xi, lam)
+        iters += 1
+        if wsa:
+            break
+    print("converged after " + str(iters) + " iterations")
+    SIR(Xi, h)
+    return Xi
+
+# %%
+# für akademisceh systeme
+%time p_simple_ident_nom = sindy_simple(X, DX_, polys, variables, th)
+calc_param_ident_error(p_nom, p_simple_ident_nom.T)
+%time p_simple_ident_zen = sindy_simple(X, x_dot, polys, variables, th)
+calc_param_ident_error(p_nom, p_simple_ident_zen.T)
+# %%
+# für wp
+basis = verkettung()
+theta = library(basis, variables, X)
+# for i in range(1, theta.shape[1]):
+#     print(np.linalg.matrix_rank(theta[:, 0:i]))
+better_basis = sel_lin_indep(theta, basis)
+%time p_simple_ident = sindy_simple(X, DX_, basis, variables, 0.5)
 
 # %%
 # a = 1.3
@@ -118,15 +281,15 @@ model = ps.SINDy(
 )
 dt= t_end/(X.shape[0]-1)
 print("nominal")
-model.fit(X, x_dot=DX_, t=dt, multiple_trajectories=False) 
-model.print()
-p_ident_nominal = model.coefficients()
+%time model.fit(X, x_dot=DX_, t=dt, multiple_trajectories=False) 
+%time model.print()
+%time p_ident_nominal = model.coefficients()
 # print(p_ident_nominal)
 
 print("Zentral")
-model.fit(X, x_dot=x_dot, t=dt, multiple_trajectories=False) 
-model.print()
-p_ident_zentral = model.coefficients()
+%time model.fit(X, x_dot=x_dot, t=dt, multiple_trajectories=False) 
+%time model.print()
+%time p_ident_zentral = model.coefficients()
 
 if NN:
     print("NN 0.1s")
